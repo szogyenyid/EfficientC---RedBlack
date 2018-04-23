99ff7e1aa4202ac23e163c0f3fa99aad82cb922f
Unknown <szogyenyid@gmail.com>
2018-04-23 19:18:08 +0200

    Coloring Inserted element

--- RedBlackTrees/Dictionary.c
+++ RedBlackTrees/Dictionary.c
@@ -42,7 +42,7 @@ void rotateLeft(DictNode* a) {
 	b->parent = a->parent;
 
 	if (a->parent == NULL) root = b;
-	else if ((a->parent->left != NULL) && isEqual(a->sajatNyelv, a->parent->left->sajatNyelv)) a->parent->left = b;
+	else if ((a->parent->left != NULL) && isEqual(a->sajatNyelv, a->parent->left->sajatNyelv)) a->parent->left = b; //itt vizsgáljuk hogy a szülõjének õ melyik oldali gyereke
 	else a->parent->right = b;
 	b->left = a;
 	a->parent = b;
@@ -60,4 +60,87 @@ void rotateRight(DictNode* b) {
 	else b->parent->right = a;
 	a->right = b;
 	b->parent = a;
+}
+
+void colorInsert(DictNode* c) {
+	DictNode *b = NULL;
+
+
+	while ((c->parent != NULL) && (c->parent->color == 'r')) { //amíg van szülõ, és az piros
+		if ((c->parent->parent->left != NULL) && isEqual(c->parent->sajatNyelv, c->parent->parent->left->sajatNyelv)) { //ha balgyerek
+			if (c->parent->parent->right != NULL) b = c->parent->parent->right; 
+			if ((b != NULL) && (b->color == 'r')) {
+				c->parent->color = 'b';
+				b->color = 'b';
+				c->parent->parent->color = 'r';
+				if (c->parent->parent != NULL) c = c->parent->parent;
+			}
+			else {
+				if ((c->parent->right != NULL) && isEqual(c->sajatNyelv,c->parent->right->sajatNyelv)) {
+					c = c->parent;
+					rotateLeft(c);
+				}
+				c->parent->color = 'b';
+				c->parent->parent->color = 'r';
+				rotateRight(c->parent->parent);
+			}
+		}
+		else {
+			if (c->parent->parent->left != NULL) b = c->parent->parent->left;
+			if ((b != NULL) && (b->color == 'r')) {
+				c->parent->color = 'b';
+				b->color = 'b';
+				c->parent->parent->color = 'r';
+				if (c->parent->parent != NULL) c = c->parent->parent;
+			}
+			else {
+				if ((c->parent->left != NULL) && isEqual(c->sajatNyelv, c->parent->left->sajatNyelv)) {
+					c = c->parent;
+					rotateRight(c);
+				}
+				c->parent->color = 'b';
+				c->parent->parent->color = 'r';
+				rotateLeft(c->parent->parent);
+			}
+		}
+
+	}
+	root->color = 'b';
+}
+void insert(char val[32]) {
+	struct DictNode *a = NULL; //x = a
+	struct DictNode *b = NULL; //y = b
+	struct DictNode *c = (DictNode*)malloc(sizeof(DictNode)); //z = c
+
+	*c->sajatNyelv = val;   //IDE KELLETT BESZURNI EGY CSILLAGOT AZ EGESZ KIFEJEZES ELÉ
+	c->left = NULL;
+	c->right = NULL;
+	c->color = 'r';
+	a = root;
+
+	if (search(val) == 1) {
+		printf("\nWord is already in the tree\n");
+		return;
+	}
+
+	if (root == NULL) {
+		root = z;
+		root->color = 'b';
+		z->parent = NULL;
+		return;
+	}
+
+	while (x != NULL) {
+		y = x;
+		if (z->key < x->key) x = x->left;
+		else x = x->right;
+	}
+
+	z->parent = y;
+	if (y == NULL) root = z;
+	else {
+		if (z->key < y->key) y->left = z;
+		else y->right = z;
+	}
+	colorInsert(z);
 }
